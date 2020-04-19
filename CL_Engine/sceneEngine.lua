bc17065309c7395b36987bbc69107d83b8f7a893
Crypto Logiq <cryptologiq@gmail.com>
2020-04-20 01:30:36 +0200

    SceneManager Updated
    Load ability self

--- CL_Engine/sceneEngine.lua
+++ CL_Engine/sceneEngine.lua
@@ -17,18 +17,25 @@ function sceneEngine:setScene(name)
   for i = 1, #self.scenes do
     if name == self.scenes[i].name then
       self.current = self.scenes[i]
+      if not self.current.module.loadScene then
+        self.current.module.load()
+      end
       return
     end
   end
+
   print("ereur, la scene "..name.." n'existe pas !")
 end
 --
 
-sceneEngine.load = function(self)
+function sceneEngine:load()
   if not self.current then
   else
     if self.current.module.load ~= nil then
-      self.current.module.load()
+      if not self.current.module.loadScene then
+        self.current.module.load()
+        self.current.module.loadScene = true
+      end
     end
   end
   --
